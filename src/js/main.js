2bca977635d133b4c69f4a54873c059303bf2dce
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2021-04-13 14:24:20 +0200

    Added manifest/serviceworker link to all pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-//See if the browser supports Service Workers, if so try to register one
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service-worker.js").then(function(registering){
+// See if the browser supports Service Workers, if so try to register one
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function (registering) {
       // Registration was successful
-      console.log("Browser: Service Worker registration is successful with the scope",registering.scope);
-    }).catch(function(error){
-      //The registration of the service worker failed
-      console.log("Browser: Service Worker registration failed with the error",error);
+      console.log(
+        "Browser: Service Worker registration is successful with the scope",
+        registering.scope
+      );
+    })
+    .catch(function (error) {
+      // The registration of the service worker failed
+      console.log(
+        "Browser: Service Worker registration failed with the error",
+        error
+      );
     });
-  } else {
-    //The registration of the service worker failed
-    console.log("Browser: I don't support Service Workers :(");
-  }
+} else {
+  // The registration of the service worker failed
+  console.log("Browser: I don't support Service Workers :(");
+}
