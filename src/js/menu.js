7631f05eb16fd2bdb491c2f800adc3772dbd1bd8
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2021-04-14 14:26:13 +0200

    Optimized JS

--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-burgermenu = document.getElementById("burgermenu");
-menu = document.getElementById("menu");
-
-burgermenu.onclick = function() {
-    burgermenu.classList.toggle("open");
-    menu.classList.toggle("menu-open")
-}
