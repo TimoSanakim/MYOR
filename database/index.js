af817b38ac10ad3840124a60815ae88a6e75304d
Luc Swinkels <luc.swinkels@live.nl>
2021-05-11 11:58:32 +0200

    branch test

--- database/index.js
+++ database/index.js
@@ -1,22 +1,23 @@
 const path = require("path");
 
-
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyparser = require('body-parser');
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+var bodyparser = require("body-parser");
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyparser.json());
-var student = require('./routes/student');
+var student = require("./routes/student");
 
-app.use('/api/kamers/', student);
+app.use("/api/kamers/", student);
 app.get("*", express.static(path.dirname(require.main.filename)));
 
-
 app.listen(process.env.port || 1234, function (error) {
-    if (error)
-        console.log(error);
+  if (error) console.log(error);
 
-    console.log('running server on port 1234');
+  console.log("running server on port 1234");
 });
+
+//test
