35a0ce09d9f05131fd7e7b771e292ec9fe1ca271
Timo <44565@student.fontys.nl>
2021-05-12 12:11:33 +0200

    fixed timer so that both timers work

--- src/js/display_Koppla-Av/before_battle.js
+++ src/js/display_Koppla-Av/before_battle.js
@@ -31,8 +31,8 @@ function fill() {
 }
 
 // Waiting timer
-var timerWaiting = document.getElementById("waiting-timer");
-minutesWaiting = 10; // Time limit in minutes
+var timerWaiting = document.getElementsByClassName("waiting-timer")[0];
+minutesWaiting = 5; // Time limit in minutes
 counterWaiting = minutesWaiting * 60; // Timer in seconds
 function secondsToMinutes(d) {
   d = Number(d);
@@ -48,4 +48,21 @@ var waitingTimer = setInterval(function () {
   if (counterWaiting == -1) {
     window.location.href = "/battle/display_votes/during_battle";
   }
+}, 1000); // Tick down every second
+
+// Waiting timer2
+var timerWaiting2 = document.getElementsByClassName("waiting-timer2")[0];
+minutesWaiting2 = 5; // Time limit in minutes
+counterWaiting2 = minutesWaiting2 * 60; // Timer in seconds
+function secondsToMinutes(d) {
+  d = Number(d);
+  var m = Math.floor((d % 3600) / 60); // Calculate minutes
+  var s = Math.floor((d % 3600) % 60); // Calculate seconds
+  return ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2); // Format in MM:SS format
+}
+var waitingTimer2 = setInterval(function () {
+  if (counterWaiting2 === -1) return clearInterval(waitingTimer2);
+  timerWaiting2.innerHTML = secondsToMinutes(counterWaiting2); // Place current counter value in HTML
+  counterWaiting2--;
+  // Alert for timer
 }, 1000); // Tick down every second
